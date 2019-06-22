ccbf26c4fbc1d4b0b8fe4bc4005e786879143009
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2019-06-22 15:27:21 +1200

    added list focus

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,21 @@ use command_input::Command;
 use command_input::CommandInput;
 use source_list::SourceList;
 
+use std::rc::Rc;
+use std::cell::RefCell;
+
 const ADD_SOURCE_KEY: i32 = 105; // 'i' key
+const FOCUS_SOURCE_LIST_KEY: i32 = 108; // 'l' key
 const ACTION_ADD_SOURCE: &'static str = "add_source";
 
 struct App {
-  source_list: SourceList,
+  source_list: Rc<RefCell<SourceList>>,
   command_input: CommandInput,
 }
 
 impl App {
   pub fn new() -> App {
-    let source_list = SourceList::new();
+    let source_list = Rc::new(RefCell::new(SourceList::new()));
     let command_input = CommandInput::new();
     App {
       source_list: source_list,
@@ -27,12 +31,12 @@ impl App {
     }
   }
   pub fn start(&mut self) {
-    self.source_list.render();
+    self.source_list.borrow().render();
     self.command_input.render();
-    let mut slc = self.source_list.clone();
+    let source_list_clone = self.source_list.clone();
     self.command_input.on_command(move |command: Command| {
       match command.name {
-        ACTION_ADD_SOURCE => slc.add_source(command.value),
+        ACTION_ADD_SOURCE => source_list_clone.borrow_mut().add_source(command.value),
         _ => (),
       };
     });
@@ -42,6 +46,9 @@ impl App {
         ADD_SOURCE_KEY => {
           self.command_input.set_command_name(ACTION_ADD_SOURCE);
           self.command_input.prompt("Add source: ");
+        }
+        FOCUS_SOURCE_LIST_KEY => {
+          self.source_list.borrow_mut().handle_focus();
         }
         _ => (),
       };
--- src/source_list.rs
+++ src/source_list.rs
@@ -1,18 +1,16 @@
 use feed_parser::parser;
 use ncurses::*;
 
-#[derive(Clone)]
 pub struct Source {
   url: String,
-  title: String
+  title: String,
 }
 
-#[derive(Clone)]
 pub struct SourceList {
   win: WINDOW,
   sources: Vec<Source>,
   width: i32,
-  height: i32,
+  selected_index: i32,
 }
 
 impl SourceList {
@@ -24,11 +22,12 @@ impl SourceList {
     let width = 30 * screen_w / 100;
     let height = screen_h - 1;
     let win = newwin(height, width, 0, 0);
+    keypad(win, true);
     SourceList {
       win: win,
       sources: Vec::new(),
       width: width,
-      height: height,
+      selected_index: -1,
     }
   }
 
@@ -36,9 +35,34 @@ impl SourceList {
     if let Some(feed) = parser::from_url(&source) {
       self.sources.push(Source {
         title: feed.title.unwrap_or(source.clone()),
-        url: source.clone()
+        url: source.clone(),
       });
-      self.render_sources();
+      self.render();
+    }
+  }
+
+  pub fn handle_focus(&mut self) {
+    self.selected_index = 0;
+    self.render();
+    loop {
+      let ch = wgetch(self.win);
+      match ch {
+        KEY_UP => {
+          if self.selected_index > 0 {
+            self.selected_index -= 1;
+          }
+        }
+        KEY_DOWN => {
+          if self.selected_index < self.sources.len() as i32 - 1 {
+            self.selected_index += 1;
+          }
+        }
+        KEY_ENTER => {
+          break;
+        }
+        _ => {}
+      };
+      self.render();
     }
   }
 
@@ -54,7 +78,13 @@ impl SourceList {
         } else {
           source.to_string()
         };
-        mvwaddstr(self.win, line, 1, &formatted_source);
+        if self.selected_index == line - 1 {
+          wattr_on(self.win, A_REVERSE());
+          mvwaddstr(self.win, line, 1, &formatted_source);
+          wattr_off(self.win, A_REVERSE());
+        } else {
+          mvwaddstr(self.win, line, 1, &formatted_source);
+        }
         line += 1;
       }
     }
@@ -62,6 +92,7 @@ impl SourceList {
   }
 
   pub fn render(&self) {
+    wclear(self.win);
     box_(self.win, 0, 0);
     wattr_on(self.win, A_BOLD());
     mvwaddstr(self.win, 0, 1, "Sources");
