1db7b39d10c1bf37fd59a850882d043dd25129c7
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2019-06-23 03:04:05 +1200

    added format html for better readability

--- src/article_viewer.rs
+++ src/article_viewer.rs
@@ -1,10 +1,12 @@
 use feed_parser::Entry;
+use html2text::from_read;
 use ncurses::*;
 use readability::extractor;
 
 pub struct ArticleViewer {
   win: WINDOW,
   article: Option<Entry>,
+  width: i32,
 }
 
 impl ArticleViewer {
@@ -14,6 +16,7 @@ impl ArticleViewer {
     ArticleViewer {
       win: win,
       article: None,
+      width: width,
     }
   }
 
@@ -25,13 +28,16 @@ impl ArticleViewer {
     if let Some(article) = &self.article {
       if let Some(title) = &article.title {
         wattr_on(self.win, A_BOLD());
-        mvwaddstr(self.win, 0, 1, &format!("{}\n ", title));
+        mvwaddstr(self.win, 0, 0, &format!("{}\n", title));
         wattr_off(self.win, A_BOLD());
       }
       if let Some(link) = &article.alternate.first() {
         let scraped_result = extractor::scrape(&link.href);
         match scraped_result {
-          Ok(scraped_data) => waddstr(self.win, &scraped_data.text),
+          Ok(scraped_data) => waddstr(
+            self.win,
+            &from_read(scraped_data.content.as_bytes(), self.width as usize),
+          ),
           Err(_) => waddstr(self.win, "Failed to fetch article"),
         };
       }
